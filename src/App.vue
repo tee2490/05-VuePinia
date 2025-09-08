c7329e42ba6dde8df3543e19eec4a520cb7f8b78
Teeradet <tee2490@hotmail.com>
2025-09-08 19:41:43 +0700

    14 routing in action

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,26 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">Hello World</div>
-  </header>
+  <div class="text-center pt-3 text-success h3">CountOPedia</div>
+  <hr />
+  <div class="row">
+    <div class="col-4 offset-1">
+      <router-link
+        class="form-control badge rounded-pill text-bg-warning p-4"
+        aria-current="page"
+        to="/"
+        >Home</router-link
+      >
+    </div>
+    <div class="col-4 offset-1">
+      <router-link
+        class="form-control badge rounded-pill text-bg-warning p-4"
+        aria-current="page"
+        to="/info"
+        >Info</router-link
+      >
+    </div>
+  </div>
 
   <RouterView />
 </template>
